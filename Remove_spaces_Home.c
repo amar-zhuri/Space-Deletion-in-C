b49782ad358c67e65a156c70d4385c8d4b78ad9c
amar-zhuri <130097411+amar-zhuri@users.noreply.github.com>
2024-03-03 01:49:34 +0100

    Add files via upload

---
+++ Remove_spaces_Home.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *delete_spaces(char *original)
+{
+    char *result = (char *) calloc (
+        strlen(original) + 1, sizeof(char)
+    );      
+
+    int position = 0;
+    for(int i = 0; i<strlen(original); i++)
+    {
+        if(original[i] != ' '){
+            result[position] = original[i];
+            position++;
+        }
+    }
+    return result;
+
+}
+
+
+
+int main(){
+
+    char line[101];
+
+    gets(line);
+    int n = atoi(line);
+    
+    for(int i = 0 ; i<n ; i++){
+    gets(line);
+    char *result = delete_spaces(line);
+    puts(result);
+    free(result);
+    }
+
+
+
+    return 0;
+}
